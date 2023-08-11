53ac9d155bad0c231cd4c5716dafae76cf0fc3fb
benbtw <58542595+benbtw@users.noreply.github.com>
2023-08-10 20:04:17 -0500

    ..

---
+++ banking system/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Helo";
+}
+
+/*
+1. main menu screen
+2. deposit/withdraw
+3. create account (checking or saving)
+4. check balance
+5. potential login section where you can do things without needing to retype info in
+*/
